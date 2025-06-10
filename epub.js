6c87a988f935329a2ea85e75bdaf3f3e4f2a9854
Huang Xin <chrox.huang@gmail.com>
2025-06-10 14:41:30 +0800

    Handle invalid href

--- epub.js
+++ epub.js
@@ -95,7 +95,7 @@ const childGetter = (doc, ns) => {
 const resolveURL = (url, relativeTo) => {
     try {
         // replace %2c in the url with a comma, this might be introduced by calibre
-        url = url.replace(/%2c/, ',')
+        url = url.replace(/%2c/gi, ',').replace(/%3a/gi, ':')
         if (relativeTo.includes(':') && !relativeTo.startsWith('OEBPS')) return new URL(url, relativeTo)
         // the base needs to be a valid URL, so set a base URL and then remove it
         const root = 'https://invalid.invalid/'
